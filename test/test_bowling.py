786cb5d00f337678b7c62a7a4e5f1144fc1952a0
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-28 16:35:24 +0200

    test if game consists of 10 frames

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from bowling import BowlingGame
 from bowling_error import BowlingError
@@ -6,6 +7,20 @@ from frame import Frame
 
 
 class TestBowlingGame(unittest.TestCase):
+    def setUp(self):
+        self.game = BowlingGame()
 
-    def test_something(self):
-        pass
+    def test_game_consists_of_10_frames(self):
+        for i in range(10):
+            first_throw = random.randint(0, 10)
+
+            if first_throw == 10:
+                second_throw = 0
+            else:
+                second_throw = random.randint(0, first_throw)
+
+            self.game.add_frame(Frame(first_throw, second_throw))
+
+        self.assertEqual(len(self.game.frames), 10)
+
+
