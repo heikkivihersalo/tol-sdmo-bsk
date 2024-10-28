8e16775b52bf5e37c78a75d5f64c4ab56920fbf2
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-28 17:15:02 +0200

    refactor to use default game init

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -8,57 +8,38 @@ from frame import Frame
 
 class TestBowlingGame(unittest.TestCase):
     def setUp(self):
-        pass
+        self.default_game = BowlingGame()
+
+        self.default_game.add_frame(Frame(1, 5))
+        self.default_game.add_frame(Frame(3, 6))
+        self.default_game.add_frame(Frame(7, 2))
+        self.default_game.add_frame(Frame(3, 6))
+        self.default_game.add_frame(Frame(4, 4))
+        self.default_game.add_frame(Frame(5, 3))
+        self.default_game.add_frame(Frame(3, 3))
+        self.default_game.add_frame(Frame(4, 5))
+        self.default_game.add_frame(Frame(8, 1))
+        self.default_game.add_frame(Frame(2, 6))
 
     def test_game_created(self):
-        game = BowlingGame()
+        special_game = BowlingGame()
 
         frame = Frame(1, 2)
-        game.add_frame(frame)
-        self.assertEqual(game.get_frame_at(0), frame)
+        special_game.add_frame(frame)
+        self.assertEqual(special_game.get_frame_at(0), frame)
 
     def  test_empty_game(self):
-        game = BowlingGame()
-        self.assertRaises(BowlingError, game.get_frame_at, 0)
+        special_game = BowlingGame()
+        self.assertRaises(BowlingError, special_game.get_frame_at, 0)
 
     def test_game_created_10_frames(self):
-        game = BowlingGame()
-
-        game.add_frame(Frame(1, 5))
-        game.add_frame(Frame(3, 6))
-        game.add_frame(Frame(7, 2))
-        game.add_frame(Frame(3, 6))
-        game.add_frame(Frame(4, 4))
-        game.add_frame(Frame(5, 3))
-        game.add_frame(Frame(3, 3))
-        game.add_frame(Frame(4, 5))
-        game.add_frame(Frame(8, 1))
-        game.add_frame(Frame(2, 6))
-
-        self.assertEqual(len(game._frames), 10)
+        self.assertEqual(len(self.default_game._frames), 10)
 
     def test_game_created_11_frames(self):
-        game = BowlingGame()
-
-        game.add_frame(Frame(1, 5))
-        game.add_frame(Frame(3, 6))
-        game.add_frame(Frame(7, 2))
-        game.add_frame(Frame(3, 6))
-        game.add_frame(Frame(4, 4))
-        game.add_frame(Frame(5, 3))
-        game.add_frame(Frame(3, 3))
-        game.add_frame(Frame(4, 5))
-        game.add_frame(Frame(8, 1))
-        game.add_frame(Frame(2, 6))
-
-        self.assertRaises(BowlingError, game.add_frame, Frame(3, 6))
+        self.assertRaises(BowlingError, self.default_game.add_frame, Frame(3, 6))
 
     def test_game_index_negative(self):
-        game = BowlingGame()
-
-        self.assertRaises(BowlingError, game.get_frame_at, -1)
+        self.assertRaises(BowlingError, self.default_game.get_frame_at, -1)
 
     def test_game_index_out_of_bounds(self):
-        game = BowlingGame()
-
-        self.assertRaises(BowlingError, game.get_frame_at, 11)
+        self.assertRaises(BowlingError, self.default_game.get_frame_at, 11)
